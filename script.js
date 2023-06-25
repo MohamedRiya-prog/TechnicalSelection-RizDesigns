05e760a2f7e60d1ba7228c6133a11bdef495c42c
MohamedRiya-prog <125153479+MohamedRiya-prog@users.noreply.github.com>
2023-06-25 23:46:29 +0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+})
+document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+        return false;
+    }
+};
 function calculateAirflow() {
     var cfm = parseFloat(document.getElementById("CFM").value);
     var lps = parseFloat(document.getElementById("LPS").value);
